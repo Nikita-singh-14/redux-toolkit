3a6ef2586d0ca11ce2c8ef3a00944f6f853a216c
Nikita-singh-14 <nk2371174@gmail.com>
2026-01-09 16:40:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Todos from './components/Todos'
+import AddTodo from './components/addTodo'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>learn about redux-toolkit</h2>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
---
+++ src/features/todo/todoslice.js
@@ -0,0 +1,39 @@
+import { createSlice,nanoid } from "@reduxjs/toolkit";
+const initialState = {
+    todos:[{id:1, text:"hello world"}]
+};
+
+
+function sayhello(){
+    console.log("hello guys...")
+}
+export const todoSlice = createSlice({
+    name:'todo',
+    initialState,
+    reducers:{
+        addTodo: (state, action) => {
+            const todo = {
+                id:nanoid(), 
+                text:action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        updateTodo: (state, action) => {
+            const{id, text} = action.payload;
+            const existingTodo = state.todos.find((todo) => todo.id === id);
+            if(existingTodo){
+                existingTodo.text = text;
+            }
+            state.todos = state.todos.find((todo) => todo.id == action.payload)
+        },
+        deleteTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+    }
+});
+
+export const {addTodo, removeTodo, updateTodo, deleteTodo} = todoSlice.actions
+export default todoSlice.reducer
